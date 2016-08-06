21c2f6f2adaba7d63b8915a1008dc8f9c024e7eb
Bruce Bowyer-Smyth <bbowyersmyth@live.com.au>
2016-08-06 14:47:22 +1000

    CLSCompliant attribute. packOptions summary

--- src/StringSpanBuilder/Properties/AssemblyInfo.cs
+++ src/StringSpanBuilder/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Bruce Bowyer-Smyth. All rights reserved.
 // Licensed under the MIT License. See the LICENSE.txt file in the project root for more information.
 
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -18,6 +19,7 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
 [assembly: InternalsVisibleTo("StringSpanBuilder.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001004d098e152d547fca0b8d092f6805daf47cfbce4748e42a15ed6e097e380620b395da3d40f720c3d420e10a85ff820ebb6928c0710a8491d06190de7c38e536a597108f3eb52132ab0b7b6bb441134a5bdfaae24432c0c3aeddd84f36be0a0f4dfa4b06d8d0d2e55031229ece0dd64104403a1d1cfb770b0715c86856e76887ce")]
 
 [assembly: AssemblyVersion("0.1.0.0")]
--- src/StringSpanWriter/Properties/AssemblyInfo.cs
+++ src/StringSpanWriter/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Bruce Bowyer-Smyth. All rights reserved.
 // Licensed under the MIT License. See the LICENSE.txt file in the project root for more information.
 
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -18,6 +19,7 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
 
 [assembly: AssemblyVersion("0.1.0.0")] 
 [assembly: AssemblyFileVersion("0.1.0.0")]
